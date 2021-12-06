cb8627dfcb7d053c8ca930a9a99d0596e2ab3faf
haixiangyan <haixiang6123@gmail.com>
2021-12-07 00:59:03 +0800

    chore: 使用 .js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
-{
-  "root": true,
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "extends": [
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
-  "rules": {
+  rules: {
     "no-console": "off",
     "no-debugger": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/ban-types": "off",
-    "camelcase": "off",
+    "no-undef": "off",
+    camelcase: "off",
     "no-empty-pattern": "off",
-    "@typescript-eslint/adjacent-overload-signatures": "off"
-  }
-}
+    "@typescript-eslint/adjacent-overload-signatures": "off",
+  },
+};
