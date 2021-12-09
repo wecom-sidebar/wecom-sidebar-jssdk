c379d31c1e430ba4a5ae792386e4c13e3bda32d3
haixiangyan <haixiang6123@gmail.com>
2021-12-09 17:06:57 +0800

    fix: 修复不能 npm run start 的问题

--- jest.config.js
+++ jest.config.js
@@ -169,7 +169,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$"],
+  transformIgnorePatterns: ["/node_modules/", "\\.pnp\\.[^\\/]+$", "/example/"],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
