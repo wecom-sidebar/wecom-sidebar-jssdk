095b60c235af9806c68ae89d9ae3062a3088ea20
haixiangyan <haixiang6123@gmail.com>
2021-12-16 01:20:34 +0800

    feat: 添加下划线

--- example/src/react-app-env.d.ts
+++ example/src/react-app-env.d.ts
@@ -1,16 +1,16 @@
 /// <reference types="react-scripts" />
-import {JsSDK} from "./lib/jsSdk";
+import { JsSDK } from "./lib/jsSdk";
 
 declare global {
   interface Window {
     // 外部控制是否为 mock
-    isMock?: boolean;
+    _isMock?: boolean;
     // mock userId
-    mockUserId: string,
+    _mockUserId: string;
     // wx.invoke 里的 Mock 关系表，apiName -> result
-    invokeResMock?: Partial<JsSDK>;
+    _invokeResMock?: Partial<JsSDK>;
     // 企业微信的 JsSdk 的 Mock 关系表，fnName -> result
-    wxResMock?: Partial<JsSDK>;
+    _wxResMock?: Partial<JsSDK>;
     // qiankun
     __POWERED_BY_QIANKUN__?: boolean;
     __INJECTED_PUBLIC_PATH_BY_QIANKUN__?: string;
